c6b76ec1e74e1ff28867ec9a83da5c32bffe1a88
Pavel Slama <p.slama@atlas.cz>
2018-09-11 13:57:50 +0200

    delete mutex & style

--- DynamicQueue.h
+++ DynamicQueue.h
@@ -26,25 +26,23 @@ SOFTWARE.
 #define DYNAMICQUEUE_H
 
 #include <deque>
-#include <climits>
 #include "mbed.h"
 
 template <class T, size_t N>
 class DynamicQueue {
-  public:
-    DynamicQueue();
-    virtual ~DynamicQueue(void);
+ public:
+  DynamicQueue();
+  virtual ~DynamicQueue(void);
 
-    void push_back(T value);
-    void push_front(T value);
-    T get();
-    void reset();
-    size_t size();
-    bool empty();
+  void push_back(T value);
+  void push_front(T value);
+  T get();
+  void reset();
+  size_t size();
+  bool empty();
 
-  private:
-    Mutex _mutex;
-    deque<T> _queue;
+ private:
+  deque<T> _queue;
 };
 
 template <class T, size_t N>
@@ -57,61 +55,49 @@ DynamicQueue<T, N>::~DynamicQueue(void) {
 
 template <class T, size_t N>
 T DynamicQueue<T, N>::get() {
-    if (_queue.size() == 0) {
-        return T();
-    }
+  if (_queue.size() == 0) {
+    return T();
+  }
 
-    _mutex.lock();
-    T res = _queue.front();
-    _queue.pop_front();
-    _mutex.unlock();
+  T res = _queue.front();
+  _queue.pop_front();
 
-    return res;
+  return res;
 }
 
 template <class T, size_t N>
 void DynamicQueue<T, N>::push_back(T value) {
-    _mutex.lock();
-    _queue.push_back(value);
+  _queue.push_back(value);
 
-    if (_queue.size() > N) {
-        _queue.pop_front();
-    }
-
-    _mutex.unlock();
+  if (_queue.size() > N) {
+    _queue.pop_front();
+  }
 }
 
 template <class T, size_t N>
 void DynamicQueue<T, N>::push_front(T value) {
-    _mutex.lock();
-    _queue.push_front(value);
+  _queue.push_front(value);
 
-    if (_queue.size() > N) {
-        _queue.pop_back();
-    }
-
-    _mutex.unlock();
+  if (_queue.size() > N) {
+    _queue.pop_back();
+  }
 }
 
 template <class T, size_t N>
 void DynamicQueue<T, N>::reset() {
-    _mutex.lock();
-
-    for (size_t i = 0; i < _queue.size(); ++i) {
-        _queue.pop_front();
-    }
-
-    _mutex.unlock();
+  for (size_t i = 0; i < _queue.size(); ++i) {
+    _queue.pop_front();
+  }
 }
 
 template <class T, size_t N>
 size_t DynamicQueue<T, N>::size() {
-    return _queue.size();
+  return _queue.size();
 }
 
 template <class T, size_t N>
 bool DynamicQueue<T, N>::empty() {
-    return _queue.size() == 0;
+  return _queue.size() == 0;
 }
 
-#endif
+#endif  // DYNAMICQUEUE_H
--- StaticQueue.h
+++ StaticQueue.h
@@ -29,26 +29,25 @@ SOFTWARE.
 
 template <class T, size_t N>
 class StaticQueue {
-  public:
-    StaticQueue();
-    virtual ~StaticQueue(void);
+ public:
+  StaticQueue();
+  virtual ~StaticQueue(void);
 
-    void push_back(T value);
-    T get();
-    void reset();
-    bool empty();
+  void push_back(T value);
+  T get();
+  void reset();
+  bool empty();
 
-  private:
-    size_t head_;
-    size_t tail_;
-    Mutex _mutex;
-    T buf_[(N + 1)];
+ private:
+  size_t head_;
+  size_t tail_;
+  T buf_[(N + 1)];
 };
 
 template <class T, size_t N>
 StaticQueue<T, N>::StaticQueue():
-    head_(0),
-    tail_(0) {
+  head_(0),
+  tail_(0) {
 }
 
 template <class T, size_t N>
@@ -57,45 +56,35 @@ StaticQueue<T, N>::~StaticQueue(void) {
 
 template <class T, size_t N>
 T StaticQueue<T, N>::get() {
-    _mutex.lock();
+  if (empty()) {
+    return T();
+  }
 
-    if (empty()) {
-        return T();
-    }
+  // Read data and advance the tail (we now have a free space)
+  T val = buf_[tail_];
+  tail_ = (tail_ + 1) % (N + 1);
 
-    // Read data and advance the tail (we now have a free space)
-    T val = buf_[tail_];
-    tail_ = (tail_ + 1) % (N + 1);
-
-    _mutex.unlock();
-
-    return val;
+  return val;
 }
 
 template <class T, size_t N>
 void StaticQueue<T, N>::push_back(T value) {
-    _mutex.lock();
+  buf_[head_] = value;
+  head_ = (head_ + 1) % (N + 1);
 
-    buf_[head_] = value;
-    head_ = (head_ + 1) % (N + 1);
-
-    if (head_ == tail_) {
-        tail_ = (tail_ + 1) % (N + 1);
-    }
-
-    _mutex.unlock();
+  if (head_ == tail_) {
+    tail_ = (tail_ + 1) % (N + 1);
+  }
 }
 
 template <class T, size_t N>
 void StaticQueue<T, N>::reset() {
-    _mutex.lock();
-    head_ = tail_;
-    _mutex.unlock();
+  head_ = tail_;
 }
 
 template <class T, size_t N>
 bool StaticQueue<T, N>::empty() {
-    return head_ == tail_;
+  return head_ == tail_;
 }
 
-#endif
+#endif  // STATICQUEUE_H
